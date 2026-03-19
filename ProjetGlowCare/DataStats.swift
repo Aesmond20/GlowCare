74c1a4cd5130c32508f3184c438a5f78f1804345
nanha13 <ernaouel@gmail.com>
2026-03-20 00:17:47 +0100

    écrans nana

---
+++ ProjetGlowCare/DataStats.swift
@@ -0,0 +1,18 @@
+//
+//  DataStats.swift
+//  ProjetGlowCare
+//
+//  Created by Apprenant78 on 14/03/2026.
+//
+
+import SwiftUI
+
+struct DataStats: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataStats()
+}
---
+++ ProjetGlowCare/NavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  ProjetGlowCare
+//
+//  Created by Apprenant78 on 13/03/2026.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
